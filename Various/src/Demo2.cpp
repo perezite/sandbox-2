f9c4fad86470f01a1811a407e78d7fda551e0e73
simulo <simulo2000@gmail.com>
2020-04-05 22:05:20 +0200

    Reflection wip

--- Various/src/Demo2.cpp
+++ Various/src/Demo2.cpp
@@ -2,11 +2,18 @@
 #include <sstream>
 #include <iostream>
 #include <iomanip>
+#include <vector>
 using namespace std;
 
 #define SB_NAMEOF(name) #name
 
 namespace myDemo2 {
+
+	template <class T>
+	void release(vector<T>& v) {
+		for (size_t i = 0; i < v.size(); i++)
+			delete v[i];
+	}
 
 	template <typename TBase, typename TDerived>
 	struct is_base_of // check if TBase is a base of TDerived
@@ -22,7 +29,42 @@ namespace myDemo2 {
 		static const bool value = (sizeof(test(get())) == sizeof(yes));
 	};
 
-	class Reflectable { };
+	template <class TType>
+	void write(const string& reflectorName, TType& object, ostringstream& os);
+
+	class BaseField {
+	public:
+		virtual ~BaseField() { }
+		virtual void write(const string& reflectorName, ostringstream& os) = 0;
+	};
+
+	template <class T>
+	class Field : public BaseField {
+		T& _object;
+	public:
+		Field(T& object) : _object(object) { }
+
+		virtual void write(const string& reflectorName, ostringstream& os) {
+			myDemo2::write<T>(reflectorName, _object, os);
+		}
+	};
+
+	class Reflectable { 
+		vector<BaseField*> _fields;
+	public:
+		virtual ~Reflectable() {
+			release(_fields);
+		}
+
+		template <class T>
+		void registerField(T& object) {
+			_fields.push_back(new Field<T>(object));
+		}
+
+		vector<BaseField*>& getFields() {
+			return _fields;
+		}
+	};
 
 	class MyClass : public Reflectable {
 		int _myInt;
@@ -30,17 +72,23 @@ namespace myDemo2 {
 	public:
 		void setMyInt(int myInt) { _myInt = myInt; }
 		void setMyFloat(float myFloat) { _myFloat = myFloat; }
+		MyClass() {
+			registerField(_myInt);
+			registerField(_myFloat);
+		}
 	};
 
 	class XmlWriter {
 	public:
-		static void writeReflectable(Reflectable& reflectable, ostringstream& os) {
-			os << "TODO: Write reflectable" << endl;
+		static void writeReflectable(const string& reflectorName, Reflectable& reflectable, ostringstream& os) {
+			auto fields = reflectable.getFields();
+			for (size_t i = 0; i < fields.size(); i++) 
+				fields[i]->write(reflectorName, os);
 		}
 
 		template <class T>
 		static void write(T& object, ostringstream& os) {
-			os << object;
+			os << object << endl;
 		}
 	};
 
@@ -54,28 +102,28 @@ namespace myDemo2 {
 
 	template <class TReflector, class TType>
 	struct Writer<TReflector, TType, false> {
-		static void write(TType& object, ostringstream& os) {
+		static void write(const string& reflectorName, TType& object, ostringstream& os) {
 			TReflector::template write<TType>(object, os);
 		}
 	};
 
 	template <class TReflector, class TType>
 	struct Writer<TReflector, TType, true> {
-		static void write(TType& reflectable, ostringstream& os) {
-			TReflector::writeReflectable(reflectable, os);
+		static void write(const string& reflectorName, TType& reflectable, ostringstream& os) {
+			TReflector::writeReflectable(reflectorName, reflectable, os);
 		}
 	};
 
 	template <class TReflector, class TType>
-	void write(TType& object, ostringstream& os) {
+	void writeField(const string& reflectorName, TType& object, ostringstream& os) {
 		const bool isReflectable = is_base_of<Reflectable, TType>::value;
-		Writer<TReflector, TType, isReflectable>::write(object, os);
+		Writer<TReflector, TType, isReflectable>::write(reflectorName, object, os);
 	}
 
 	template <class TType>
 	void write(const string& reflectorName, TType& object, ostringstream& os) {
 		if (reflectorName == SB_NAMEOF(XmlWriter)) {
-			write<XmlWriter, TType>(object, os);
+			writeField<XmlWriter, TType>(reflectorName, object, os);
 		}
 	}
 
