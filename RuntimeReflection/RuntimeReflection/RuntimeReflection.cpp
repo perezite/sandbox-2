b1957d1bf59cbcc064fa7a9b772ffb26cd88fbd0
Prez <simulo2000@gmail.com>
2021-05-25 23:09:05 +0200

    Runtime reflection wip

--- RuntimeReflection/RuntimeReflection/RuntimeReflection.cpp
+++ RuntimeReflection/RuntimeReflection/RuntimeReflection.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-class Vector2f {
+template <class T>
+void deleteAll(T& t)
+{
+    for (typename T::iterator it = t.begin(); it != t.end(); it++)
+        delete (*it);
+}
+
+class Vector2f100 {
 public: 
     float x, y;
 
-    Vector2f(float x_ = 0, float y_ = 0)
+    Vector2f100(float x_ = 0, float y_ = 0)
         : x(x_), y(y_)
     { }
 };
 
 class Class100 {
 public:
-    Vector2f position;
+    Vector2f100 position;
 
     int health;
 
@@ -29,21 +37,25 @@ public:
     float test;
 };
 
-void indent100(int level) {
-    const int spaces = level * 4;
+void indent100(size_t level) {
+    const size_t spaces = level * 4;
     for (size_t i = 0; i < spaces; i++)
         cout << " ";
 }   
 
 /*
-void printMetadata100(Metadata& md, int level = 0) {
+void printMetadata100(Metadata& md, size_t level = 0) {
     for (size_t i = 0; i < md.countOwnFields(); i++) { 
         Metadata field = md.getOwnFields()[i];
-        if (field.isComposite())
+        if (field.isComposite()) {
+            cout << indent100(level) << field.getName()
+                 << " (" << field.getTypename() << "):" << endl; 
             printMetadata(field, level + 1);
-        else if (field.isSimple())
+        else if (field.isSimple()) {
             cout << indent100(level) << field.getName() 
-                 << ": " << field.toString() << endl;
+                 << " (" << field.getTypename() << "): "
+                 << field.toString() << endl;
+        }
     }
 }
 */
@@ -51,13 +63,13 @@ void printMetadata100(Metadata& md, int level = 0) {
 void demo100() {
     // Reflection reflection;
     // reflection
-    //     .beginClass<Vector2f>("Vector2f")
+    //     .beginClass<Vector2f100>("Vector2f100")
     //         .addField("x", x)
     //         .addField("y", y)
     //     .endClass()
     //     .beginClass<Class100>("Class100")
-    //         .addField(position)
-    //         .addField(health)
+    //         .addField("position", position)
+    //         .addField("health", health)
     //     .endClass()
     //     .deriveClass<Derived100, Class100>("Derived100")
     //         .addField("test", test)
@@ -73,9 +85,89 @@ void demo100() {
     //     if (md.getFields()[i].isOfType<int>())
     //         cout << md.getFields()[i].getName() << " os of type int" << endl;
     // }
+
+
+    // Output
+    // Derived
+    // 
+}
+
+class Metadata2
+{
+    string _name;
+
+public:
+    Metadata2(const string& name) : _name(name) { }
+
+    template <class T> void addField(const string& name, T& field) {
+
+    }
+};
+
+template <class T> class ClassMetadataBuilder2;
+
+class Reflection2
+{
+    vector<Metadata2*> _metadataList;
+
+public:
+    virtual ~Reflection2() { deleteAll(_metadataList); }
+
+    template <class T> ClassMetadataBuilder2<T> beginClass(const string& name) {
+        return ClassMetadataBuilder2<T>(*this, name);
+    }
+};
+
+template <class T>
+class ClassMetadataBuilder2
+{
+    Reflection2& _parent;
+
+    Metadata2 _metadata;
+
+public:
+    ClassMetadataBuilder2(Reflection2& parent, const string& name) : _parent(parent), _metadata(name) { }
+
+    template <class T>
+    ClassMetadataBuilder2& addField(const string& name, T& field) {
+        Metadata2 metadata(name);
+
+        //_metadata.addField(name, field);
+    }
+};
+
+
+class MyClass2 {
+public:
+    int x;
+
+    MyClass2() : x(42) { }
+};
+
+void demo2() {
+    Reflection2 reflection;
+    // reflection
+    //     .beginClass<MyClass1>("MyClass1");
+    //         .addField("x", x)    
+    //     .endClass();
+
+    MyClass2 myClass;
+
+    // Metadata md = reflection.getMetadata(myClass);
+    // cout << md.getName();
+    // cout << md.getFields(0).getName() << " " 
+    //      << md.getFields(0).getTypename() << md.getFields(0).toString();
+}
+
+
+
+void demo() {
+    demo1();
+    //demo2();
+    //demo100();
 }
 
 int main() {
-    cout << "Hello World!\n";
+    demo();
     cin.get();
 }
