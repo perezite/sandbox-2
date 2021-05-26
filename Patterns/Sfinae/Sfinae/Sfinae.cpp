af655770059e4450837468bab4c01da699235154
Prez <simulo2000@gmail.com>
2021-05-26 22:10:04 +0200

    Sfinae wip

--- Patterns/Sfinae/Sfinae/Sfinae.cpp
+++ Patterns/Sfinae/Sfinae/Sfinae.cpp
@@ -2,8 +2,70 @@
 
 using namespace std;
 
+namespace d1 {
+    namespace my {
+        template<bool B, typename T = void> struct enable_if {
+        };
+
+        template<typename T> struct enable_if<true, T> {
+            typedef T type; };
+    }
+
+    template<bool B = false> struct print_boolean {
+        static void run() {
+            cout << "false" << endl;
+        }
+    };
+
+    template<> struct print_boolean<true> {
+        static void run() {
+            cout << "true" << endl;
+        }
+    };
+
+    void demo() {
+        my::enable_if<1 == 1, int>::type x = 1;
+        cout << x << endl;
+        const bool test = false;
+        print_boolean<test>::run();
+        const bool test2 = true;
+        print_boolean<true>::run();
+        cin.get();
+    }
+}
+
+namespace d2 {
+    class Base {};
+    class Derived : public Base {};
+
+    template <class Base, class Derived> struct is_base_of
+    {
+        typedef char Yes[1];
+        typedef char No[2];
+
+        static Yes& test(Base*) { }
+        static No& test(...) { }
+
+        static Derived* get() { };
+
+        static const bool value = sizeof(test(get())) == sizeof(Yes);
+
+    };
+
+    void demo() {
+        cout << is_base_of<Base, Derived>::value << endl;
+        cout << is_base_of<Derived, Base>::value << endl;
+        cout << is_base_of<int, float>::value << endl;
+
+    }
+}
+
 int main()
 {
-    cout << "Hello World!\n";
+    d2::demo();
+    //d1::demo();
     cin.get();
 }
+
+// Sources
+// https://eli.thegreenplace.net/2014/sfinae-and-enable_if/
