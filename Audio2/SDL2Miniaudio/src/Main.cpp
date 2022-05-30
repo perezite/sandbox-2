348c23cdc2a377269156f0086895a22027a21060
Prez <simulo2000@gmail.com>
2022-05-30 18:16:36 +0200

    First working draft for miniaudio on android using SDL_RWops

--- Audio2/SDL2Miniaudio/src/Main.cpp
+++ Audio2/SDL2Miniaudio/src/Main.cpp
@@ -62,7 +62,7 @@
 		
 		auto result = funopen(asset, android_read, android_write, android_seek, android_close);
 		
-		auto test = fileno(result);
+		//auto test = fileno(result);
 
 		return result;
 	}
@@ -75,17 +75,86 @@
 #define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio.h"
 #include <string>
+#include "Main.h"
 
 using namespace std;
 using namespace sb;
 
+typedef struct
+{
+	ma_vfs_callbacks cb;
+} my_vfs;
+
 ma_engine engine;
+my_vfs vfs;
+
+static ma_result my_vfs_open(ma_vfs* pVFS, const char* pFilePath, ma_uint32 openMode, ma_vfs_file* pFile)
+{
+	string mode; 
+	if ((openMode & MA_OPEN_MODE_READ) != 0) { 
+		if ((openMode & MA_OPEN_MODE_WRITE) != 0) 
+			mode = "r+";
+		else 
+			mode = "rb";
+	}
+	else 
+		mode = "wb";
+
+	SDL_RWops* handle = SDL_RWFromFile(pFilePath, mode.c_str());
+	ma_result result = handle ? MA_SUCCESS : MA_ERROR;
+
+	*pFile = handle;
+	return result;
+}
+
+static ma_result my_vfs_info(ma_vfs* pVFS, ma_vfs_file file, ma_file_info* pInfo)
+{
+	SDL_RWops* rw = (SDL_RWops*)file;
+	pInfo->sizeInBytes = SDL_RWsize(rw);
+
+	return MA_SUCCESS;
+}
+
+static ma_result my_vfs_read(ma_vfs* pVFS, ma_vfs_file file, void* pDst, size_t sizeInBytes, size_t* pBytesRead)
+{
+	size_t result;
+
+	SDL_ClearError();
+	SDL_RWops* rw = (SDL_RWops*)file;
+	result = SDL_RWread(rw, pDst, 1, sizeInBytes);
+
+	if (pBytesRead != NULL) 
+		*pBytesRead = result;
+	
+	if (result == 0)
+	{
+		const char* check = SDL_GetError();
+		bool hasError = check != NULL && strlen(check) > 0;
+		return hasError ? MA_ERROR : MA_AT_END;
+	}
+
+	return MA_SUCCESS;
+}
+
+static ma_result my_vfs_close(ma_vfs* pVFS, ma_vfs_file file)
+{
+	SDL_RWclose((SDL_RWops*)file);
+
+	return MA_SUCCESS;
+}
+
 
 void playSound(const string& path)
 {
-    ma_result result;
+	vfs.cb.onOpen = my_vfs_open;
+	vfs.cb.onInfo = my_vfs_info;
+	vfs.cb.onRead = my_vfs_read;
+	vfs.cb.onClose = my_vfs_close;
 
-    result = ma_engine_init(NULL, &engine);
+    ma_result result;
+	ma_engine_config config = ma_engine_config_init();
+	config.pResourceManagerVFS = &vfs;
+	result = ma_engine_init(&config, &engine);
     SB_ERROR_IF(result != MA_SUCCESS, "Failed to initialize audio engine.");
 
     result = ma_engine_play_sound(&engine, path.c_str(), NULL);
