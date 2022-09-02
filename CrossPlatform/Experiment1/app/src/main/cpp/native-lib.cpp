fd112b0018e5d49f6012cadd9cbbabcc68bbb822
Prez <simulo2000@gmail.com>
2022-09-02 20:35:46 +0200

    Finish including libraries from hello-libs ndk sample

--- CrossPlatform/Experiment1/app/src/main/cpp/native-lib.cpp
+++ CrossPlatform/Experiment1/app/src/main/cpp/native-lib.cpp
@@ -1,9 +1,24 @@
-#include <jni.h>
 #include <string>
+#include <sstream>
+#include <jni.h>
+#include <gmath.h>
+#include <gperf.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_organisation_app_MainActivity_stringFromJNI(JNIEnv* env, jobject /* this */) 
 {		
-    std::string hello = "Hello from my C++ App";
-    return env->NewStringUTF(hello.c_str());
+    auto ticks = GetTicks();
+
+	unsigned result = 0;
+    for (auto exp = 0; exp < 32; ++exp) {
+        volatile unsigned val = gpower(exp);
+		result = val;
+    }
+	
+    ticks = GetTicks() - ticks;
+
+	std::ostringstream os;
+	os << "Result from C++: " << result << ". Calculation took: " << ticks << " ticks"; 
+	std::string str = os.str();
+    return env->NewStringUTF(str.c_str());
 }
