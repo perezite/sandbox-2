44539b5c62595a26abf6eb1e5fc500c406ba4cf9
Prez <simulo2000@gmail.com>
2022-09-09 22:58:42 +0200

    Show SDL Version in app

--- CrossPlatform/Experiment2/app/src/main/cpp/native-lib.cpp
+++ CrossPlatform/Experiment2/app/src/main/cpp/native-lib.cpp
@@ -8,8 +8,13 @@ using namespace std;
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_organisation_app_MainActivity_stringFromJNI(JNIEnv* env, jobject /* this */) 
 {			
+	SDL_version version;
+	SDL_GetVersion(&version);
+	char sdlVersion[1024];
+	sprintf(sdlVersion, "%u.%u.%u", version.major, version.minor, version.patch);
+
 	ostringstream os;
-	os << "Hello from NDK. Was SDL Video initialized: " << SDL_WasInit(SDL_INIT_VIDEO);
+	os << "Hello from NDK with SDL Version: " << sdlVersion;
 	
     return env->NewStringUTF(os.str().c_str());
 }
