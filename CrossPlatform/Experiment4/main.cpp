1939c7c91e82503fd493e6d381e7635c33c1514d
Prez <simulo2000@gmail.com>
2022-09-28 22:00:21 +0200

    Add additional cmake build configuration (release with debug info)

--- CrossPlatform/Experiment4/main.cpp
+++ CrossPlatform/Experiment4/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 using namespace std;
 
+#ifdef NDEBUG
+	#define MY_RELEASE
+#else
+	#define MY_DEBUG
+#endif
+
 void main()
 {
+	#if defined(MY_RELEASE)
+		cout << "Release Build" << endl;
+	#elif defined(MY_DEBUG)
+		cout << "Debug Build" << endl;
+	#endif
+
 	auto test = 42;
 	cout << "test" << endl;
+
+	cin.get();
 }
