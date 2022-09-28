0fcbcc501dd1b6cae80f0bc38e0529334f10a5c5
Prez <simulo2000@gmail.com>
2022-09-28 22:54:37 +0200

    minor

--- CrossPlatform/Experiment4/main.cpp
+++ CrossPlatform/Experiment4/main.cpp
@@ -11,11 +11,11 @@ using namespace std;
 
 void main()
 {
-	#if defined(MY_RELEASE)
-		cout << "Release Build" << endl;
-	#elif defined(MY_DEBUG)
-		cout << "Debug Build" << endl;
-	#endif
+#if defined(MY_RELEASE)
+	cout << "Release Build" << endl;
+#elif defined(MY_DEBUG)
+	cout << "Debug Build" << endl;
+#endif
 
 	cout << add(1, 2) << endl;
 
