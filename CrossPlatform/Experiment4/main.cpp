20ae793ac4769420cf14f9eb041a8e1c6807d995
Prez <simulo2000@gmail.com>
2022-09-28 23:41:00 +0200

    Minor

--- CrossPlatform/Experiment4/main.cpp
+++ CrossPlatform/Experiment4/main.cpp
@@ -9,15 +9,31 @@ using namespace std;
 	#define MY_DEBUG
 #endif
 
-void main()
+int main()
 {
 #if defined(MY_RELEASE)
 	cout << "Release Build" << endl;
 #elif defined(MY_DEBUG)
 	cout << "Debug Build" << endl;
 #endif
-	
+
 	cout << add(1, 2) << endl;
 
+#ifdef _MSVC_LANG
+	if (_MSVC_LANG == 201703L) std::cout << "C++17\n";
+	else if (_MSVC_LANG == 201402L) std::cout << "C++14\n";
+	else if (_MSVC_LANG == 201103L) std::cout << "C++11\n";
+	else if (_MSVC_LANG == 199711L) std::cout << "C++98\n";
+	else std::cout << "pre-standard C++\n";
+#endif
+
+	if (__cplusplus == 201703L) std::cout << "C++17\n";
+	else if (__cplusplus == 201402L) std::cout << "C++14\n";
+	else if (__cplusplus == 201103L) std::cout << "C++11\n";
+	else if (__cplusplus == 199711L) std::cout << "C++98\n";
+	else std::cout << "pre-standard C++\n";
+
 	cin.get();
+
+	return 0;
 }
