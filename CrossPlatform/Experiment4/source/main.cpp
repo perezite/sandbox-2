b8a2201581a25336fe4d2ec7e8049943db28352b
Prez <simulo2000@gmail.com>
2022-09-29 20:43:45 +0200

    Restructure CMake folders

--- CrossPlatform/Experiment4/source/main.cpp
+++ CrossPlatform/Experiment4/source/main.cpp
@@ -9,16 +9,18 @@ using namespace std;
 	#define MY_DEBUG
 #endif
 
-int main()
+void printBuildConfiguration()
 {
 #if defined(MY_RELEASE)
 	cout << "Release Build" << endl;
 #elif defined(MY_DEBUG)
 	cout << "Debug Build" << endl;
 #endif
+}
 
-	cout << add(1, 2) << endl;
-
+void printVisualStudioCppVersion()
+{
+	cout << "Visual Studio C++ Version: ";
 #ifdef _MSVC_LANG
 	if (_MSVC_LANG == 201703L) std::cout << "C++17\n";
 	else if (_MSVC_LANG == 201402L) std::cout << "C++14\n";
@@ -26,12 +28,26 @@ int main()
 	else if (_MSVC_LANG == 199711L) std::cout << "C++98\n";
 	else std::cout << "pre-standard C++\n";
 #endif
+}
 
+void printCppVersion()
+{
+	cout << "Standard C++ Version: ";
 	if (__cplusplus == 201703L) std::cout << "C++17\n";
 	else if (__cplusplus == 201402L) std::cout << "C++14\n";
 	else if (__cplusplus == 201103L) std::cout << "C++11\n";
 	else if (__cplusplus == 199711L) std::cout << "C++98\n";
 	else std::cout << "pre-standard C++\n";
+}
+
+int main()
+{
+	printBuildConfiguration();
+
+	cout << add(1, 2) << endl;
+
+	printVisualStudioCppVersion();
+	printCppVersion();
 
 	cin.get();
 
