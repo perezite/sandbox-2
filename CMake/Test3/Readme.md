97e16cc658783bffc393167441426327d8751698
Prez <simulo2000@gmail.com>
2024-02-18 19:06:41 +0100

    Cmake wip

---
+++ CMake/Test3/Readme.md
@@ -0,0 +1,1 @@
+Basic cmake setup. Can only be started from top level folder
---
+++ CMake/Test3/o2-pong/source/o2-pong/main.cpp
@@ -0,0 +1,28 @@
+﻿#include "lib.h"
+#include <iostream>
+using namespace std;
+using namespace lib;
+
+namespace app
+{
+    string getBuildConfigurationInfo()
+    {
+        #ifdef _DEBUG
+            return "debug build";
+        #endif
+    
+        #ifdef NDEBUG
+            return "release build";
+        #endif
+    }
+}
+
+int main()
+{
+    cout << "o2-app " << app::getBuildConfigurationInfo() << endl;
+    cout << "lib " << lib::getBuildConfigurationInfo() << endl;
+
+    cin.get();
+
+    return 0;
+}
