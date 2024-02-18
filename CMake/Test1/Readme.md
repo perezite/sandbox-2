d35f8f9c8cf68badaba8c7e0b255ac9d79611a6d
Prez <simulo2000@gmail.com>
2024-02-18 15:52:34 +0100

    Cmake tests wip

---
+++ CMake/Test1/Readme.md
@@ -0,0 +1,2 @@
+Basic Cmake setup 
+Can be started from top-level- or sub-folder
---
+++ CMake/Test2/o2-pong/source/o2-pong/main.cpp
@@ -0,0 +1,25 @@
+﻿#include "window.h"
+#include <iostream>
+using namespace std;
+using namespace o2;
+
+string getBuildTypeInfo()
+{
+    #ifdef _DEBUG
+        return "debug build";
+    #endif
+    
+    #ifdef NDEBUG
+        return "release build";
+    #endif
+}
+
+int main()
+{
+    cout << "o2-app " << getBuildTypeInfo() << endl;
+
+    Window window;
+    cin.get();
+    
+    return 0;
+}
