201884f54926aef68559b044cab188bcd0957de2
Prez <simulo2000@gmail.com>
2024-02-18 15:02:07 +0100

    add cmake config for release mode

--- CMake/Test1/o2-pong/source/o2-pong/main.cpp
+++ CMake/Test1/o2-pong/source/o2-pong/main.cpp
@@ -3,8 +3,21 @@
 using namespace std;
 using namespace o2;
 
+string getDebugModeInfo()
+{
+    #ifdef _DEBUG
+        return "debug mode";
+    #endif
+    
+    #ifdef NDEBUG
+        return "release mode";
+    #endif
+}
+
 int main()
 {
+    cout << "o2-app " << getDebugModeInfo() << endl;
+
     Window window;
     cin.get();
     
